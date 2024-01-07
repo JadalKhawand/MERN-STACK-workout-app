35b6729b508a825b000a11b628b879c438e343c3
Jad alKhawand <jkh42005@gmail.com>
2024-01-07 19:08:42 +0200

    Protecting API Routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -8,8 +8,13 @@ const {
   updateWorkout
 } = require('../controllers/workoutController')
 
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
+
+// require auth for all workout routes
+router.use(requireAuth)
+
 // get all workouts
 router.get('/',getWorkouts)
 
