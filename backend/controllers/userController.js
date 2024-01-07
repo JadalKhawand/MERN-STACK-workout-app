649ece3abbf0f1eb135b98239e42df979179166f
Jad alKhawand <jkh42005@gmail.com>
2024-01-07 15:53:22 +0200

    fixed login typo and tested on postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ const createToken = (_id) => {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.signup(email, password);
+    const user = await User.login(email, password);
     // create token
     const token = createToken(user._id);
     res.status(200).json({ email, token });
