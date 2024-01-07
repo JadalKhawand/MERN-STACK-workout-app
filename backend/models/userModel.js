cc3bc1b29b31b57b0d8a198a29a9f4ec329e2ccb
Jad alKhawand <jkh42005@gmail.com>
2024-01-07 15:41:19 +0200

    creating a login schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -47,6 +47,17 @@ userSchema.statics.signup = async function (email, password){
 // static login method 
 userSchema.statics.login = async function(email,password){
 
+  if(!email || !password){
+    throw Error('All fields must be filled')
+  }
+  const user = await this.findOne({email})
+  if(!user){
+    throw Error('Incorret email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+}
 
 
 module.exports = mongoose.model('User', userSchema)
